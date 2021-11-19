ec0552c70609d3d24e1cfeabc48500a1e099ec74
Yicoding <18063229116@163.com>
2021-11-19 11:03:07 +0800

    配置

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/kaiyu.kong/Ecode/2021/eco-web-site/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
