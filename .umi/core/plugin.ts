fa5b5075417cdec7a535b7acd3a66f12b0d473a9
Yicoding <18063229116@163.com>
2021-11-19 10:47:38 +0800

    add

---
+++ .umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/kaiyu.kong/Ecode/2021/eco-web-site/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
+});
+
+export { plugin };
---
+++ .umi/dumi/demos/index.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import React from 'react';
+import { dynamic } from 'dumi';
+
+export default {
+  'bash-demo': {
+    component: function DumiDemo() {
+  var React;
+},
+    previewerProps: {"sources":{"_":{"jsx":""}},"dependencies":{},"identifier":"bash-demo"},
+  },
+};
